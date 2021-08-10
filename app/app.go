ab167af81eb8d8bffc2879160f4e08a6c2f166cf
Abhinav Gupta <abhinav.gupta@thoughtworks.com>
2021-08-10 11:15:10 +0530

    Step-1 c1

--- app/app.go
+++ app/app.go
@@ -6,12 +6,27 @@ import (
 )
 
 func StartServer() {
-	if err := http.ListenAndServe(":8080", pingHandler()); err != nil && err != http.ErrServerClosed {
+	http.HandleFunc("/test", testBook)
+	http.HandleFunc("/book", handleBook)
+	//if err := http.ListenAndServe(":8080", pingHandler()); err != nil && err != http.ErrServerClosed {
+	if err := http.ListenAndServe(":8080", nil); err != nil && err != http.ErrServerClosed {
 		fmt.Errorf("something went wrong %s", err)
 		fmt.Println("Server Not Started")
 		return
 	}
 	fmt.Println("Server Started")
+}
+
+func handleBook(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusCreated)
+	// writer.Write("Ticket created")
+	// fmt.Println("Book Created!")
+}
+
+func testBook(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusInternalServerError)
+	// writer.Write("Ticket created")
+	// fmt.Println("Book Created!")
 }
 
 func pingHandler() http.HandlerFunc {
